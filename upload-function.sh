bd166f3cb3e28207170a8307a0a690988bbdd37d
Takayoshi Makabe <makabe.takayoshi1117@gmail.com>
2021-04-24 01:05:44 +0900

    [Improve] ストップワードを別ファイルとして切り出し

--- upload-function.sh
+++ upload-function.sh
@@ -7,5 +7,5 @@ zip lambda.zip lambda_function.py
 aws lambda update-function-code --function-name baseballJanome --zip-file fileb://lambda.zip
 
 cd ../baseballCreateWordCloud
-zip lambda.zip lambda_function.py
+zip lambda.zip lambda_function.py stop_word.py
 aws lambda update-function-code --function-name baseballCreateWordCloud --zip-file fileb://lambda.zip
